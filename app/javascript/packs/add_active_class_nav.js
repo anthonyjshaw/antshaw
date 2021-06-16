56a12a81855e736ecc09478bcce03da970fb5b68
Anthony Shaw <anthonyshaw@sceaga.com>
2021-06-16 09:10:58 +0100

    Added coloring

--- app/javascript/packs/add_active_class_nav.js
+++ app/javascript/packs/add_active_class_nav.js
@@ -1,14 +1,12 @@
 const addActiveClassNav = () => {
   const page = window.location.href;
   const active = document.getElementById('home-active')
-  console.log(page);
   const navLinks = document.querySelectorAll('.nav-link');
-  console.log(navLinks);
+
   navLinks.forEach((element) => {
     if (page.includes(element.innerHTML.toLowerCase())) {
-      console.log(element.innerHTML.toLowerCase());
-      active.classList.remove('active')
-      element.classList.add('active');
+          active.classList.remove('active')
+          element.classList.add('active');
     }
   })
 }
