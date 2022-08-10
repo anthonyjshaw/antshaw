739b058d65d0006cd2491c6547b513781ca2dd2f
Anthony Shaw <anthonyshaw@sceaga.com>
2022-08-10 07:41:33 +0100

    Added different pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
+  get 'contact', to: 'pages#contact', as: :contact
   get 'home', to: 'pages#home'
   get 'portfolio', to: 'pages#portfolio', as: :portfolio
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
