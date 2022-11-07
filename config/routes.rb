e7a23b383e94784aedcae88fdcf9118f96674e53
Anthony Shaw <anthonyshaw@sceaga.com>
2022-11-07 16:09:18 +0000

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get "/", to: 'pages#home'
-
-  constraints subdomain: 'copywriting' do 
-    root to: 'pages#home_copywriting', as: :home_copywriting
-    get '/about', to: 'pages#about', as: :about
-    get '/contact', to: 'pages#contact', as: :contact
-    get '/blog', to: 'pages#blog', as: :blog
-    get '/portfolio', to: 'pages#portfolio', as: :portfolio
-  end
+  root to: 'pages#home_copywriting', as: :home_copywriting
+  get '/about', to: 'pages#about', as: :about
+  get '/contact', to: 'pages#contact', as: :contact
+  get '/blog', to: 'pages#blog', as: :blog
+  get '/portfolio', to: 'pages#portfolio', as: :portfolio
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
