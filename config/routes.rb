edf60357cc01a63ded1f1b53e89f9eb320f5c8ff
Anthony Shaw <anthonyshaw@sceaga.com>
2022-11-07 15:49:38 +0000

    Added new subdomin

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   constraints subdomain: 'copywriting' do 
     root to: 'pages#home_copywriting', as: :home_copywriting
     get '/about', to: 'pages#about', as: :about
-    get 'contact', to: 'pages#contact', as: :contact
-    get 'blog', to: 'pages#blog', as: :blog
-    get 'portfolio', to: 'pages#portfolio', as: :portfolio
+    get '/contact', to: 'pages#contact', as: :contact
+    get '/blog', to: 'pages#blog', as: :blog
+    get '/portfolio', to: 'pages#portfolio', as: :portfolio
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
